bfcc7de4d72e8b0fe8e31b611865adf0ca75d311
Jinyoung Ahn <yj6210ag@gmail.com>
2020-08-06 17:48:21 +0900

    file upload

--- c_programming/200806/200806/main.c
+++ c_programming/200806/200806/main.c
@@ -2,17 +2,119 @@
 #include <stdlib.h>
 #include <time.h>
 #define MAX_NUM 6
+#define MAX_MAN 3
+#define STU_MAX 3
 
 void lotto(); // 1. 로또 프로그램
 int MyRand(int max); // 2. 랜덤 난수 생성
 void MyNameGen(int len); // 3. 랜덤 문자열 생성
 char* MyNameGen2(int len); // 4. 랜덤 문자열 생성2
+void ascii(); // 5. 아스키 코드 출력
+void ascii2(); // 5. 아스키 코드 출력
+int CC(int i);
+
+typedef struct person {
+	char name[20];
+	int age;
+	int a, b, c;
+} ps;
 
 int main() {
-	char* s;
-	s = MyNameGen2(10);
-	printf("%s", s);
+	ascii2();
+
 	return 0;
+}
+
+int CC(int i) {
+	if ((i == 7) || (i == 8) || (i == 9) || (i == 10) || (i == 11) || (i == 13)) {
+		return 32;
+	}
+	return 0;
+}
+
+void ascii2() {
+	int i;
+	int i1;
+	int col = 5;
+	int offset = (128 + (col - 1)) / col;
+	FILE *fp = fopen("E:\\programming\\c_programming\\test.txt", "wb");
+
+	for (i = 0; i < 26; i++) {
+		if ((i == 1)) {
+			fprintf(fp, "%3d  %3x  %3c  |  ", i, i, i);
+			fprintf(fp, "%3d  %3x  %3c  |  ", i + 26, i + 26, ' ');
+			fprintf(fp, "%3d  %3x  %3c  |  ", i + 52, i + 52, i + 52);
+			fprintf(fp, "%3d  %3x  %3c  |  ", i + 78, i + 78, i + 78);
+			fprintf(fp, "%3d  %3x  %3c\n", i + 104, i + 104, i + 104);
+		}
+		else if ((i == 7) || (i == 8) || (i == 9) || (i == 10) || (i == 11) || (i == 13)) {
+			fprintf(fp, "%3d  %3x  %3c  |  ", i, i, ' ');
+			fprintf(fp, "%3d  %3x  %3c  |  ", i + 26, i + 26, i + 26);
+			fprintf(fp, "%3d  %3x  %3c  |  ", i + 52, i + 52, i + 52);
+			fprintf(fp, "%3d  %3x  %3c  |  ", i + 78, i + 78, i + 78);
+			fprintf(fp, "%3d  %3x  %3c\n", i + 104, i + 104, i + 104);
+		}
+		else if ((i == 24) || (i == 25)) {
+			fprintf(fp, "%3d  %3x  %3c  |  ", i, i, i);
+			fprintf(fp, "%3d  %3x  %3c  |  ", i + 26, i + 26, i + 26);
+			fprintf(fp, "%3d  %3x  %3c  |  ", i + 52, i + 52, i + 52);
+			fprintf(fp, "%3d  %3x  %3c  |\n", i + 78, i + 78, i + 78);
+		}
+		else {
+			fprintf(fp, "%3d  %3x  %3c  |  ", i, i, i);
+			fprintf(fp, "%3d  %3x  %3c  |  ", i + 26, i + 26, i + 26);
+			fprintf(fp, "%3d  %3x  %3c  |  ", i + 52, i + 52, i + 52);
+			fprintf(fp, "%3d  %3x  %3c  |  ", i + 78, i + 78, i + 78);
+			fprintf(fp, "%3d  %3x  %3c\n", i + 104, i + 104, i + 104);
+		}
+	}
+
+	/*for (i = 0; i < offset; i++) {
+		i1 = i;
+		fprintf(fp, "%3d %3x %3c | ", i1, i1, CC(i1)); i1 += offset;
+		fprintf(fp, "%3d %3x %3c | ", i1, i1, CC(i1)); i1 += offset;
+		fprintf(fp, "%3d %3x %3c | ", i1, i1, CC(i1)); i1 += offset;
+		fprintf(fp, "%3d %3x %3c | ", i1, i1, CC(i1)); i1 += offset;
+		if (i1 < 128) {
+			fprintf(fp, "%3d %2x %3c | ", i1, i1, CC(i1));
+		}
+		fprintf(fp, "\n");
+	}*/
+
+}
+
+void ascii() {
+	int i;
+
+	for (i = 0; i < 26; i++) {
+		if ((i == 1)) {
+			fprintf("%3d  %3x  %3c  |  ", i, i, i);
+			fprintf("%3d  %3x  %3c  |  ", i + 26, i + 26,  ' ');
+			fprintf("%3d  %3x  %3c  |  ", i + 52, i + 52, i + 52);
+			fprintf("%3d  %3x  %3c  |  ", i + 78, i + 78, i + 78);
+			fprintf("%3d  %3x  %3c\n", i + 104, i + 104, i + 104);
+		}
+		else if ((i == 7) || (i == 8) || (i == 9) || (i == 10) || (i == 11) || (i == 13)) {
+			fprintf("%3d  %3x  %3c  |  ", i, i, ' ');
+			fprintf("%3d  %3x  %3c  |  ", i + 26, i +26, i + 26);
+			fprintf("%3d  %3x  %3c  |  ", i + 52, i + 52, i + 52);
+			fprintf("%3d  %3x  %3c  |  ", i + 78, i + 78, i + 78);
+			fprintf("%3d  %3x  %3c\n", i + 104, i + 104, i + 104);
+		}
+		else if ((i == 24) || (i == 25)) {
+			fprintf("%3d  %3x  %3c  |  ", i, i, i);
+			fprintf("%3d  %3x  %3c  |  ", i + 26, i + 26, i + 26);
+			fprintf("%3d  %3x  %3c  |  ", i + 52, i + 52, i + 52);
+			fprintf("%3d  %3x  %3c  |\n", i + 78, i + 78, i + 78);
+		}
+		else { 
+			fprintf("%3d  %3x  %3c  |  ", i, i, i);
+			fprintf("%3d  %3x  %3c  |  ", i + 26, i + 26, i + 26);
+			fprintf("%3d  %3x  %3c  |  ", i + 52, i + 52, i + 52);
+			fprintf("%3d  %3x  %3c  |  ", i + 78, i + 78, i + 78);
+			fprintf("%3d  %3x  %3c\n", i + 104, i + 104, i + 104);
+		}
+	}
 }
 
 char str[100];
